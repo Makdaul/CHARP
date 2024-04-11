a2678ca1f5a20181d08293ea08ecb9a479df41ef
Mokujin <yakovlevmi@inbox.ru>
2024-04-11 18:46:23 +0300

    lessone2: Task1,2,3,4

---
+++ Lesson/Lessone2/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length) // или n
+{
+    arr[i] = i +1;
+    // System.Console.Write(arr[i]);
+    // System.Console.Write(' ');
+    System.Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
