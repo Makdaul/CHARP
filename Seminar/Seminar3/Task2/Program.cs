749f5436b0d20b2a6d775ba3eb49be5f596bb405
Mokujin <yakovlevmi@inbox.ru>
2024-04-11 21:08:37 +0300

    add Seminar3: Task1,2

---
+++ Seminar/Seminar3/Task2/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да 
+
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);//[min, max] [0, 9]
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+bool FindNumInArray(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Задайте массив из 10 элементов, заполненный числами из
+// промежутка [-10, 10]. Замените отрицательные элементы на
+// положительные, а положительные на отрицательные.
+// Пример
+// [1 -5 6]
+// => [-1 5 -6]
+
+int[] ChangeArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+    return array;
+}
+
+
+System.Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value of arr element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value of arr element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(size, min, max);
+PrintArray(array);
+
+PrintArray(ChangeArray(array));
+
+// System.Console.Write("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine(FindNumInArray(array, num));
+
