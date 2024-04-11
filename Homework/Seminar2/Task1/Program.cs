3eb8f5c4de23f0a2b0de9aacd751ac6757665066
Mokujin <yakovlevmi@inbox.ru>
2024-04-11 16:23:20 +0300

    add homework

---
+++ Homework/Seminar2/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Input number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (a % 7 == 0 && a % 23 == 0)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
---
+++ Homework/Seminar2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите координату X: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите координату Y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+        if (x > 0 && y > 0)
+            Console.WriteLine("1");
+        else if (x < 0 && y > 0)
+            Console.WriteLine("2");
+        else if (x < 0 && y < 0)
+            Console.WriteLine("3");
+        else if (x > 0 && y < 0)
+            Console.WriteLine("4");
+        else
+            Console.WriteLine("Точка находится на оси координат");
+    }
+}
