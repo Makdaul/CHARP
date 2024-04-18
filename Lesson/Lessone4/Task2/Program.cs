e324d9d76546eb2e4f6abfdee925883bb2fdfaf9
Mokujin <yakovlevmi@inbox.ru>
2024-04-18 18:14:58 +0300

    add Lessone4

---
+++ Lesson/Lessone4/Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿int[,] CreateMatrix(int rowCount, int colCount)
+{
+    int[,] matrix = new int[rowCount, colCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 1000);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4);
+PrintMatrix(matrix);
+foreach (int e in matrix)
+{
+    if (IsInteresting(e) == true)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+bool IsInteresting(int value)
+{
+    int SumOfDigits = GetSumOfDigits(value);
+    if (SumOfDigits % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int GetSumOfDigits(int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum = sum + value % 10;
+        value = value /= 10;
+    }
+    return sum;
+}
